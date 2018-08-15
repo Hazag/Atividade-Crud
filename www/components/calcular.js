4e1655446391f11a6c4003b14ef15cd59569565c
Hazag <davicosta97@gmail.com>
2018-08-16 02:47:46 +0900

    Project's Initial Commit

---
+++ www/components/calcular.js
@@ -0,0 +1,7 @@
+$(document).on("click","#somar", function(){
+var valor1 = parseInt($("#valor1").val());
+var valor2 = parseInt($("#valor2").val());
+
+var result = valor1 + valor2;
+$("#result").val(result);
+});
